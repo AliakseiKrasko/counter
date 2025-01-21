bdf225d73d496aaf23373cc67c762bc4a72bfcf3
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-01-21 14:46:01 +0100

    finished tests all reducer

--- src/state/CounterReducer.test.ts
+++ src/state/CounterReducer.test.ts
@@ -1,4 +1,11 @@
-import {counterReducer, incrementAC, resetCounterAC} from './CounterReducer';
+import {
+    counterReducer, EnableSetButtonAC,
+    incrementAC,
+    resetCounterAC,
+    setCounterAC,
+    SetMaxErrorCounterAC, SetStartErrorCounterAC,
+    setValuesAC
+} from './CounterReducer';
 
 
 test('reset counter ', () => {
@@ -23,6 +30,77 @@ test('increment', () => {
     const endState = counterReducer(startState, incrementAC())
     expect(endState.counter).toBe(6)
     expect(endState.isSetDisabled).toBe(false)
+    expect(endState.startError).toBe(false)
+    expect(endState.startValue).toBe(2)
+    expect(endState.maxError).toBe(false)
+
+});
+
+test('set_values counter', () => {
+    const startState = {counter: 0, maxValue: 10, startValue: 2,
+        isSetDisabled: false,
+        maxError: false,
+        startError: false};
+    const endState = counterReducer(startState, setValuesAC(8, 4))
+    expect(endState.counter).toBe(4)
+    expect(endState.isSetDisabled).toBe(true)
+    expect(endState.maxValue).toBe(8)
+    expect(endState.startError).toBe(false)
+    expect(endState.startValue).toBe(4)
+    expect(endState.maxError).toBe(false)
+
+});
+test('set_counter', () => {
+    const startState = {counter: 0, maxValue: 10, startValue: 2,
+        isSetDisabled: false,
+        maxError: false,
+        startError: false};
+    const endState = counterReducer(startState, setCounterAC(5))
+    expect(endState.counter).toBe(5)
+    expect(endState.isSetDisabled).toBe(false)
+    expect(endState.maxValue).toBe(10)
+    expect(endState.startError).toBe(false)
+    expect(endState.startValue).toBe(2)
+    expect(endState.maxError).toBe(false)
+
+});
+test('set_max_error_counter', () => {
+    const startState = {counter: 0, maxValue: 10, startValue: 2,
+        isSetDisabled: false,
+        maxError: false,
+        startError: false};
+    const endState = counterReducer(startState, SetMaxErrorCounterAC(true))
+    expect(endState.counter).toBe(0)
+    expect(endState.isSetDisabled).toBe(false)
+    expect(endState.maxValue).toBe(10)
+    expect(endState.startError).toBe(false)
+    expect(endState.startValue).toBe(2)
+    expect(endState.maxError).toBe(true)
+
+});
+test('set_start_error_counter', () => {
+    const startState = {counter: 0, maxValue: 10, startValue: 2,
+        isSetDisabled: false,
+        maxError: false,
+        startError: false};
+    const endState = counterReducer(startState, SetStartErrorCounterAC(true))
+    expect(endState.counter).toBe(0)
+    expect(endState.isSetDisabled).toBe(false)
+    expect(endState.maxValue).toBe(10)
+    expect(endState.startError).toBe(true)
+    expect(endState.startValue).toBe(2)
+    expect(endState.maxError).toBe(false)
+
+});
+test('enable_set_button', () => {
+    const startState = {counter: 0, maxValue: 10, startValue: 2,
+        isSetDisabled: false,
+        maxError: false,
+        startError: false};
+    const endState = counterReducer(startState, EnableSetButtonAC(true))
+    expect(endState.counter).toBe(0)
+    expect(endState.isSetDisabled).toBe(true)
+    expect(endState.maxValue).toBe(10)
     expect(endState.startError).toBe(false)
     expect(endState.startValue).toBe(2)
     expect(endState.maxError).toBe(false)
--- src/state/CounterReducer.ts
+++ src/state/CounterReducer.ts
@@ -16,7 +16,7 @@ const initialState: StateType = {
     startError: false,
 };
 
-type ActionType = Increment | ResetCounter;
+
 
 export type Increment = {
     type: 'INCREMENT';
@@ -27,6 +27,30 @@ export type ResetCounter = {
     counter: number;
 };
 
+export type SetValues = {
+    type: 'SET_VALUES';
+    maxValue: number;
+    startValue: number
+};
+export type SetCounter = {
+    type: 'SET_COUNTER';
+    startValue: number
+};
+export type SetMaxErrorCounter = {
+    type: 'SET_MAX_ERROR';
+    maxError: boolean
+};
+export type SetStartErrorCounter = {
+    type: 'SET_START_ERROR';
+    startError: boolean
+};
+export type EnableSetButton = {
+    type: 'ENABLE_SET_BUTTON';
+    isSetDisabled: boolean
+};
+
+
+type ActionType = Increment | ResetCounter | SetValues | SetCounter | SetMaxErrorCounter | SetStartErrorCounter | EnableSetButton;
 
 
 export const counterReducer = (state: StateType = initialState, action: ActionType): StateType => {
@@ -35,6 +59,22 @@ export const counterReducer = (state: StateType = initialState, action: ActionTy
             return { ...state, counter: state.counter + 1 };
         case 'RESET_COUNTER':
             return { ...state, counter: state.startValue, isSetDisabled: false };
+        case 'SET_VALUES':
+            return {
+                ...state,
+                maxValue: action.maxValue,
+                startValue: action.startValue,
+                counter: action.startValue,
+                isSetDisabled: true,
+            };
+        case 'SET_COUNTER':
+            return { ...state, counter: action.startValue };
+        case 'SET_MAX_ERROR':
+            return { ...state, maxError: action.maxError };
+        case 'SET_START_ERROR':
+            return { ...state, startError: action.startError };
+        case 'ENABLE_SET_BUTTON':
+            return { ...state, isSetDisabled: action.isSetDisabled };
         default:
             return state; // Возвращаем текущее состояние, если действие не распознано
     }
@@ -45,9 +85,23 @@ export const counterReducer = (state: StateType = initialState, action: ActionTy
 export const incrementAC = (): Increment => {
     return {type: 'INCREMENT'}
 }
-
 export const resetCounterAC = (counter: number): ResetCounter => {
     return {type: 'RESET_COUNTER', counter: counter}
+}
+export const setValuesAC = (maxValue: number, startValue: number): SetValues => {
+    return {type: 'SET_VALUES', maxValue: maxValue, startValue: startValue}
+}
+export const setCounterAC = (startValue: number): SetCounter => {
+    return {type: 'SET_COUNTER', startValue: startValue}
+}
+export const SetMaxErrorCounterAC = (maxError: boolean): SetMaxErrorCounter => {
+    return {type: 'SET_MAX_ERROR', maxError: maxError}
+}
+export const SetStartErrorCounterAC = (startError: boolean): SetStartErrorCounter => {
+    return {type: 'SET_START_ERROR', startError: startError}
+}
+export const EnableSetButtonAC = (isSetDisabled: boolean): EnableSetButton => {
+    return {type: 'ENABLE_SET_BUTTON', isSetDisabled: isSetDisabled}
 }
 
 
